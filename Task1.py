49b7f872daef2d116ce064b4905f3a50df22e042
Sumit Kumar <sumitshaw2037@gmail.com>
2025-11-06 13:12:50 +0530

    Task commit.

---
+++ Task1.py
@@ -0,0 +1,8 @@
+#Takes two numbers as input from the user.
+first_number=int(input("Enter the first number:"))
+second_number=int(input("Enter the second number:"))
+#Addition
+print("Addition: ", first_number + second_number)
+print("Subtraction: ", first_number - second_number)
+print("Multiplication: ", first_number * second_number)
+print("Division: ", first_number / second_number)
---
+++ Task2.py
@@ -0,0 +1,5 @@
+#Takes a user's first name and last name as input.
+first_name = input("Enter your first name:")
+last_name = input("Enter your last name:")
+#Concatenates the first name and last name into a full name.
+print("Hello,", first_name ,last_name,"! welcome to the python program.")
